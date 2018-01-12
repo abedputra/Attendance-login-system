8304b6991a84e8f7b2effe3592149af8829d55e3
Abed Putra <abedputra@gmail.com>
2018-01-12 11:00:41 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://visit2indonesia.com/AdminAbsent/'; //change link
+$config['base_url'] = 'https://your_website.com'; //change link
 
 /*
 |--------------------------------------------------------------------------
