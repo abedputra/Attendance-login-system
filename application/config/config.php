b1e0a565a2d44aaa74d8bec366e2d54bfc1db88e
Abed Putra <abedputra@gmail.com>
2018-01-12 11:01:34 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://your_website.com'; //change link
+$config['base_url'] = 'http://adminweb.com/admin/'; //change link
 
 /*
 |--------------------------------------------------------------------------
