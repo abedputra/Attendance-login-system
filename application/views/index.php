2933cd54115af99fa3641e1b9352d1e9939c226d
Abed Putra <abedputra@gmail.com>
2018-07-20 21:45:12 +0700

    disable info

--- application/views/index.php
+++ application/views/index.php
@@ -1,3 +1,8 @@
+    <?php
+    //check user level
+    $dataLevel = $this->userlevel->checkLevel($role);
+    //check user level
+    ?>
     <div class="jumbotron">
         <div class="container">
           <h1>Hi, <?php echo $first_name; ?>!</h1>
@@ -16,37 +21,42 @@
         $allLatePerc = "0%";
     }
     ?>
-    <div class="panel panel-primary">
-      <div class="panel-heading">
-       <h3 class="panel-title">Your Employees Today 
-            <div class="pull-right">
-                <?php echo $nowToday; ?>
-            </div>
-        </h3>
-      </div>
-      <div class="panel-body">
-        <div class="panel-body">
-           <div class="row">
-               <div class="col-md-6">
-                    <div class="progress">
-                      <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" 
-                        aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $allabsetPerc; ?>">
-                        
-                      </div>
-                    </div>
-                    <h4 class="progress-label"><?php echo $allabsetPerc; ?> or <?php echo $count_absent_today; ?> Person Absent Today.</h4>
+    <?php
+        if($dataLevel == 'is_admin'){
+    ?>
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+           <h3 class="panel-title">Your Employees Today 
+                <div class="pull-right">
+                    <?php echo $nowToday; ?>
                 </div>
-                <div class="col-md-6">
-                    <div class="progress">
-                      <div class="progress-bar progress-bar-danger progress-bar-striped active" role="progressbar" 
-                        aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $allLatePerc; ?>">
-                        
-                      </div>
+            </h3>
+          </div>
+          <div class="panel-body">
+            <div class="panel-body">
+               <div class="row">
+                   <div class="col-md-6">
+                        <div class="progress">
+                          <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" 
+                            aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $allabsetPerc; ?>">
+                            
+                          </div>
+                        </div>
+                        <h4 class="progress-label"><?php echo $allabsetPerc; ?> or <?php echo $count_absent_today; ?> Person Absent Today.</h4>
                     </div>
-                    <h4 class="progress-label"><?php echo $allLatePerc; ?> or <?php echo $count_late_today; ?> Employees are late Today.</h4>
+                    <div class="col-md-6">
+                        <div class="progress">
+                          <div class="progress-bar progress-bar-danger progress-bar-striped active" role="progressbar" 
+                            aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $allLatePerc; ?>">
+                            
+                          </div>
+                        </div>
+                        <h4 class="progress-label"><?php echo $allLatePerc; ?> or <?php echo $count_late_today; ?> Employees are late Today.</h4>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     
-    
+    <?php
+    }
+    ?>
