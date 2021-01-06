b33a7c818b309ce5d64d48076679c33440cb9f2d
ashish <ashish.11745@gmail.com>
2021-01-06 13:07:32 +0530

    commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.43.190/system-management/'; //change link
+$config['base_url'] = 'localhost/attendance-login-system'; //change link
 
 /*
 |--------------------------------------------------------------------------
